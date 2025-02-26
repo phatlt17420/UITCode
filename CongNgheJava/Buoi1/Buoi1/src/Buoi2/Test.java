f47daf1102397ed78e2e0c351fb5205387bf5772
phatlt17420 <phatlt17420@gmail.com>
2025-02-26 22:20:00 +0700

    Java

---
+++ CongNgheJava/Buoi1/Buoi1/src/Buoi2/Test.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Buoi2;
+
+/**
+ *
+ * @author phatlt17420
+ */
+import java.util.Scanner;
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner ip = new Scanner(System.in);
+        Double Result= ip.nextDouble();
+        System.out.printf("KetQuacuaBanChon: % \n", Result);
+    }
+    
+}
---
+++ CongNgheJava/Buoi1/Buoi1_BaiTapQuaTrinh/src/Buoi1/DemoDatatypeOperator.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Buoi1;
+
+/**
+ *
+ * @author phatlt17420
+ */
+public class DemoDatatypeOperator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 2; 
+        int y = 3;
+        System.out.println(x - y);
+        
+    }
+    
+}
