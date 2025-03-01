860d6d8bae70ad939b3f6e755d3fc1cf5cfc58c1
phatlt17420 <phatlt17420@gmail.com>
2025-03-01 21:41:24 +0700

    20250301

---
+++ CongNgheJava/Buoi1/Buoi1/src/Buoi4/DemoArray.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Buoi4;
+
+/**
+ *
+ * @author phatlt17420
+ */
+public class DemoArray {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] arr1 = {1, 3, 5, 6};
+
+        int[] arr2 = new int[10];
+        arr2[0] = 1;
+        arr2[1] = 2;
+        System.out.println(arr1[1]);
+    }
