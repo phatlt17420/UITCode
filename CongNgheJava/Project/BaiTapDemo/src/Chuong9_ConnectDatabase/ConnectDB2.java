81fde2e8a9f5797402e37ea39558fd399db7fe81
phatlt17420 <phatlt17420@gmail.com>
2025-04-04 20:17:12 +0700

    a

---
+++ CongNgheJava/Project/BaiTapDemo/src/Chuong9_ConnectDatabase/ConnectDB2.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Chuong9_ConnectDatabase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author phatlt17420
+ */
+public class ConnectDB2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String connectionUrl = "jdbc:sqlserver://LAPASUSTOP\\SQLEXPRESS:1433;"
+                + "databaseName=QuanLyDeTai;user=sa;password=sa;";
+        try {
+            Connection con = DriverManager.getConnection(connectionUrl);
+
+            Statement stmt = con.createStatement(); // tạo câu truy vấn
+
+            String SQL = "select * from CHUYENNGANH";
+            ResultSet rs = stmt.executeQuery(SQL);
+
+            while (rs.next()) {
+                System.out.println(rs.getString("MSCN") + " " + rs.getString("TENCN"));
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("Loi ket noi: " + ex);
+        }
+
+    }
+
+}
