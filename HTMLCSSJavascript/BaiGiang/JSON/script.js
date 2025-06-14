94df7e6de5912dfe36e221312f15be3eb6ad85d5
phatlt17420 <phatlt17420@gmail.com>
2025-06-14 17:36:58 +0700

    aa

---
+++ HTMLCSSJavascript/BaiGiang/JSON/script.js
@@ -0,0 +1,4 @@
+const myObj = { name: "Phát", age: 33, city: "HCM" };
+const myJSON = JSON.stringify(myObj);
+
+document.getElementById("demoJSON").innerHTML = myJSON;
--- HTMLCSSJavascript/Practice/script.js
+++ HTMLCSSJavascript/Practice/script.js
@@ -1,11 +1,16 @@
-const car = {
-  name: "Phat",
-  lastName: "Le Tan",
-  age: 33,
-  eyeColor: "black",
-  fullName: function () {
-    return this.lastName + this.name;
-  },
-};
+function Person(first, last, age, eye) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eye;
+}
+const myFather = new Person("John", "Doe", 50, "Blue");
+const myMother = new Person("Sally", "Rally", 48, "Green");
+Person.prototype.name = function () {};
 document.getElementById("demo").innerHTML =
-  car.fullName() + " " + car.age + car.eyeColor;
+  "My Father is " +
+  myFather.age +
+  ". My Mother is " +
+  myMother.age +
+  " ." +
+  myFather.nationality;
