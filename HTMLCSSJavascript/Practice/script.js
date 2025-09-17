95612c7030a84a43f9af567f00826d437526a4d6
phatlt17420 <phatlt17420@gmail.com>
2025-09-17 22:48:43 +0700

    dalete

--- HTMLCSSJavascript/Practice/script.js
+++ HTMLCSSJavascript/Practice/script.js
@@ -1,16 +1,5 @@
-function Person(first, last, age, eye) {
-  this.firstName = first;
-  this.lastName = last;
-  this.age = age;
-  this.eyeColor = eye;
-}
-const myFather = new Person("John", "Doe", 50, "Blue");
-const myMother = new Person("Sally", "Rally", 48, "Green");
-Person.prototype.name = function () {};
-document.getElementById("demo").innerHTML =
-  "My Father is " +
-  myFather.age +
-  ". My Mother is " +
-  myMother.age +
-  " ." +
-  myFather.nationality;
+let c = document.getElementById("myCanvas");
+let ctx = c.getContext("2d");
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 100);
+ctx.stroke();
